bbbb7f82378bd0755287774e2553a9ce8fb229e1
Park Ju Hyung <qkrwngud825@gmail.com>
2017-11-06 20:30:39 +0900

    init: patch warranty bit props
    Change-Id: I720eba8898a38a5335dbb5b39638dc9eb3aa7596

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1259,6 +1259,8 @@ static void ProcessSafetyNetProps() {
     InitPropertySet("ro.boot.verifiedbootstate", "green");
     InitPropertySet("ro.boot.veritymode", "enforcing");
     InitPropertySet("ro.boot.vbmeta.device_state", "locked");
+    InitPropertySet("ro.boot.warranty_bit", "0");
+    InitPropertySet("ro.warranty_bit", "0");
 }
 }
 
